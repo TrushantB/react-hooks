a9564846e7327ce1790b1cb7464b51acd7561d70
TrushanT <trushant.ivision@gmail.com>
2019-05-07 18:07:20 +0530

    first commit upload all code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
 import './App.css';
+import 'antd/dist/antd.css';
+import { Card ,Input } from 'antd';
+export default function App() {
 
-function App() {
+  const width=useWidth()
+  
+  const name=useInput('trush');
+  const address=useInput('sangamner')
+  useTitle(name.value+ ' ' +address.value);
+
+   function useInput(initial) {
+    const [value,setValue] = useState(initial);
+
+    function handleChange(e) {
+      setValue(e.target.value)
+    }
+    return {
+      value,
+      onChange:handleChange,
+      onMouseOver :mousehover
+    };
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <h1>Index</h1>
+       <Card style={{ width: 400 }} >
+        <Input  placeholder="Name"  {...name}/> <br/><br/>
+        <Input  placeholder="Address"  {...address}/> <br/><br/>
+        <Input  placeholder="Address"  value={width} />
+
+      </Card>
     </div>
   );
 }
+    function useWidth() {
+      const [width, setWidth]=useState(window.innerWidth);
+      useEffect(() => {
+        const resizeHandle = () => setWidth(window.innerWidth)
+        window.addEventListener('resize',resizeHandle)
+        return () => {
+          window.removeEventListener('resize',resizeHandle)
+        }
+      })
+      return width;
+    }
 
-export default App;
+    function useTitle(name){
+      useEffect(() => {
+        document.title=name;
+      })
+    }
+    function mousehover(e) {
+      document.title=e.target.value;
+    }
+
+
